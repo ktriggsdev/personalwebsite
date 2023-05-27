4e254e7d05a1463ad59714b2ec73aad7151e351b
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-05-27 17:50:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ import pandas as pd
 
 st.title("Portfolio Dashboard")
 col1, col2, col3, col4, col5 = st.columns(5)
+
+with col1:
+  st.header("Projects: 25")
+  
+with col2:
+  st.header("Project 1")
+
+with col3:
+  st.header("Project 2")
+  
+  with col4:
+  st.header("Project 3")
+  
+  with col5:
+  st.header("Project 4")
