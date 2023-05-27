697de191c590522d808598a0d62ce6d579109cb8
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-05-27 18:59:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,16 +12,16 @@ st.title("Portfolio Dashboard")
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
-  st.header("Projects: 25")
+  st.button("Projects: 25")
   
 with col2:
-  st.header("Project 1")
+  st.button("Project 1")
 
 with col3:
-  st.header("Project 2")
+  st.button("Project 2")
   
 with col4:
-  st.header("Project 3")
+  st.button("Project 3")
   
 with col5:
-  st.header("Project 4")
+  st.button("Project 4")
