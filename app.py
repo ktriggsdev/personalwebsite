5b30bd036f529452418db1479e4a5136abbcc868
Kieran Triggs <127336461+ktriggsdev@users.noreply.github.com>
2023-05-27 18:07:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # LINK TO THE CSS FILE
-with open("style.css")as f:
+with open("style.css") as f:
  st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 st.title("Portfolio Dashboard")
